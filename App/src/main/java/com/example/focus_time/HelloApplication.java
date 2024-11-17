bd6cdf4b3f7c4e72671823ba3ece4af04bd02973
faysaljadoum <faycalrachid3@gmail.com>
2024-11-17 15:33:05 +0100

    add a folder for documentation and others files

--- App/src/main/java/com/example/focus_time/HelloApplication.java
+++ App/src/main/java/com/example/focus_time/HelloApplication.java
@@ -1,6 +1,11 @@
 package com.example.focus_time;
 
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.io.IOException;
 
 import static javafx.application.Application.launch;
