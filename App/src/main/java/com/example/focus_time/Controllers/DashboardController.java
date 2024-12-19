c63895b05228624f8821cb57a31bd02a16fbe744
sihame nachid <132794406+sihamenachid@users.noreply.github.com>
2024-12-19 17:37:50 +0100

    Add files via upload

--- App/src/main/java/com/example/focus_time/Controllers/DashboardController.java
+++ App/src/main/java/com/example/focus_time/Controllers/DashboardController.java
@@ -1,24 +1,61 @@
 package com.example.focus_time.Controllers;
 
-
+import com.example.focus_time.Models.dao.ApplicationDAOImp;
+import com.example.focus_time.Models.dao.UserDAOImp;
+import com.example.focus_time.Models.domain.Application;
+import com.example.focus_time.Models.domain.User;
+import com.example.focus_time.utils.PowerShellExecutor;
+import com.example.focus_time.utils.SceneManager;
+import com.example.focus_time.utils.SerialNumberUtil;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class DashboardController {
 
     @FXML
-    private Label pageTitle;  // Label to show the page title
+    private AnchorPane parent;  // Label to show the page title
 
     @FXML
-    public void initialize() {
-        // Set the title when this page is loaded
-        pageTitle.setText("Dashboard");
+    private Label applicationsCountLabel; // The label to display the number of applications
+
+    private final ApplicationDAOImp applicationDAO;
+
+    public DashboardController() {
+        applicationDAO = new ApplicationDAOImp();
     }
 
+    public void initialize() {
+        String userSN = SerialNumberUtil.getPCSerialNumber();
+
+        User user = new User(userSN);
+
+        // Pass the connection from the ApplicationDAOImp instance
+        int applicationsCount = ApplicationDAOImp.getApplicationsCount(applicationDAO.getConnection(), user);
+        applicationsCountLabel.setText(String.valueOf(applicationsCount)); // Set the count in the label
+    }
     @FXML
-    private void goToGoals(ActionEvent event) {
-        // Navigate to the Goals page
-        //SceneManager.switchScene("Goals.fxml");
+    private void goToApplications(ActionEvent event) {
+        try {
+            // Load the Applications.fxml file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Views/Applications.fxml"));
+            Parent applicationsRoot = loader.load();
+
+            // Get the current stage
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            // Set the new scene
+            stage.setScene(new Scene(applicationsRoot));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
